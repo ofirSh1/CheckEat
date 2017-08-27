b3d0b9271c0da1df5f2341fa3f1573c04927a8fb
ofirSh1 <ofir689@gmail.com>
2017-08-27 22:34:39 +0300

    27/8

--- web/script/searchDish.js
+++ web/script/searchDish.js
@@ -7,7 +7,105 @@ $(function () {
             document.getElementById("restNearByLabel").style.display = 'none';
         });
     }
+    //autoComplete();
 });
+/*
+function autoComplete() {
+    var otherTypesList = [
+        "מנה ראשונה",
+        "מנה עיקרית",
+        "קינוח",
+        "מנה אחרונה",
+        "איטלקית",
+        "סלט",
+        "פסטה",
+        "פיצה",
+        "סינית",
+        "תאילנדית",
+        "המבורגר",
+        "ארוחת בוקר",
+        "סושי",
+        "נודלס",
+        "כריך",
+        "יפנית",
+        "סינית",
+        "סושי",
+        "חומוס",
+        "מרק",
+        "מקסיקנית",
+        "עוגה",
+        "גלידה",
+        "דג",
+        "שניצל"];
+    $("#otherTypes").autocomplete({
+        source: otherTypesList
+    });
+    var datalist = jQuery('otherTypesList');
+    var optionsarray = jQuery.map(otherTypesList ,function(option) {
+        return option.value;
+    });
+    var input = jQuery('input[list]');
+    var inputcommas = (input.val().match(/,/g)||[]).length;
+    var separator = ',';
+
+    function filldatalist(prefix) {
+        if (input.val().indexOf(separator) > -1 && options.length > 0) {
+            datalist.empty();
+            for (i=0; i < optionsarray.length; i++ ) {
+                if (prefix.indexOf(optionsarray[i]) < 0 ) {
+                    datalist.append('<option value="'+prefix+optionsarray[i]+'">');
+                }
+            }
+        }
+    }
+    input.bind("change paste keyup",function() {
+        var inputtrim = input.val().replace(/^\s+|\s+$/g, "");
+        var currentcommas = (input.val().match(/,/g)||[]).length;
+        if (inputtrim != input.val()) {
+            if (inputcommas != currentcommas) {
+                var lsIndex = inputtrim.lastIndexOf(separator);
+                var str = (lsIndex != -1) ? inputtrim.substr(0, lsIndex)+", " : "";
+                filldatalist(str);
+                inputcommas = currentcommas;
+            }
+            input.val(inputtrim);
+        }
+    });*/
+
+/*
+    var datalist = jQuery('datalist');
+    var options = jQuery('datalist option');
+    var optionsarray = jQuery.map(options ,function(option) {
+        return option.value;
+    });
+    var input = jQuery('input[list]');
+    var inputcommas = (input.val().match(/,/g)||[]).length;
+    var separator = ',';
+
+    function filldatalist(prefix) {
+        if (input.val().indexOf(separator) > -1 && options.length > 0) {
+            datalist.empty();
+            for (i=0; i < optionsarray.length; i++ ) {
+                if (prefix.indexOf(optionsarray[i]) < 0 ) {
+                    datalist.append('<option value="'+prefix+optionsarray[i]+'">');
+                }
+            }
+        }
+    }
+  input.bind("change paste keyup",function() {
+        var inputtrim = input.val().replace(/^\s+|\s+$/g, "");
+        var currentcommas = (input.val().match(/,/g)||[]).length;
+        if (inputtrim != input.val()) {
+            if (inputcommas != currentcommas) {
+                var lsIndex = inputtrim.lastIndexOf(separator);
+                var str = (lsIndex != -1) ? inputtrim.substr(0, lsIndex)+", " : "";
+                filldatalist(str);
+                inputcommas = currentcommas;
+            }
+            input.val(inputtrim);
+        }
+    });*/
+//}
 
 function findDish()
 {
